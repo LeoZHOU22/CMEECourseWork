91f212b0e0a706077fb33af7d3139a9745471a73
LeoZHOU22 <147003070+LeoZHOU22@users.noreply.github.com>
2023-10-15 19:41:11 +0100

    Add files via upload

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Author: Leo ZHOU leozhou.23@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2023
+
+echo -e "\nThis is a shell script! \n"
+
+#exit
+bash myscript.sh
