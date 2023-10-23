2898e4cace0ea3fab7acace22c6d11ffd46d1872
Leo ZHOU <leo.zhou23@imperial.ac.uk>
2023-10-23 15:35:34 +0100

    All existing file

--- week2/code/align_seqs.py
+++ week2/code/align_seqs.py
@@ -55,3 +55,24 @@ for i in range(l1): # Note that you just take the last alignment with the highes
 print(my_best_align)
 print(s1)
 print("Best score:", my_best_score)
+
+def read_sequences_from_file(filename):
+    with open(filename, 'r') as file:
+        seq1 = file.readline().strip()
+        seq2 = file.readline().strip()
+    return seq1, seq2
+
+def write_to_file(filename, s1, best_align, best_score):
+    with open(filename, 'w') as file:
+        file.write("Best Alignment:\n")
+        file.write(s1 + "\n")
+        file.write(best_align + "\n")
+        file.write("Best Score: " + str(best_score))
+
+# Reading sequences from an external file
+seq1, seq2 = read_sequences_from_file('../data/sequences.csv')
+
+# ... [The rest of the code remains unchanged till the best alignment calculation]
+
+# Write the best alignment to an output file
+write_to_file('../results/alignment_result.txt', s1, my_best_align, my_best_score)
